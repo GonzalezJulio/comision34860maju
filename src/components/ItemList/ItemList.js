10ad2fdccf43935913972f777eefb24e563968cc
sebazdev <szuviria@gmail.com>
2023-01-03 21:17:11 -0300

    Clase de promesas

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,6 +3,8 @@ const ItemList = ({ products }) => {
         <ul>
             {products.map(prod => <li key={prod.id}>{prod.name}</li>)}
             {/* {products.map(prod => <Item key={prod.id} {...prod}/>)} */}
+
+            {/* { products.map(prod => React.createElement(Item, {...prod}))} */}
         </ul> 
     )
 }
