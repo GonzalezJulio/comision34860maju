e6d1bbb27c18294a5b2671b81eb2a916be4bfec6
sebazdev <szuviria@gmail.com>
2023-01-05 21:09:56 -0300

    Clase de pais

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [input, setInput] = useState('')
+  console.log(input)
+  useEffect(() => {
+    fetch('https://api.mercadolibre.com/sites/MLA/search?q=macbook')
+      .then(response => {
+        return response.json()
+      })
+      .then(json => { 
+        setProducts(json.results)
+      })
+  }, [])
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault()
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${input}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(json => { 
+      setProducts(json.results)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+        <h1>Mercado Libre</h1>
+
+        <form onSubmit={handleOnSubmit}>
+          <input value={input} onChange={(event) => setInput(event.target.value)}/>
+          <button type="submit">Search</button>
+        </form>
+
+        <div>
+          {products.map(prod => {
+            return (
+              <div key={prod.id}>
+                <h2>{prod.title}</h2>
+                <img src={prod.thumbnail} alt={prod.title}/>
+                <h3>${prod.price}</h3>
+              </div>
+            )
+          })}
+        </div>
     </div>
   );
 }
